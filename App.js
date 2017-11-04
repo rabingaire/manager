8187e50d98098d91ee7ab204f923187ae612bdd9
Rabin Gaire <rabingaire20@gmail.com>
2017-11-04 10:32:59 +0545

    loginform added

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import {
-  View, 
-  Text
-} from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 
-import reducers from './src/reducers'
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component {
   
@@ -26,9 +23,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>Hello</Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
