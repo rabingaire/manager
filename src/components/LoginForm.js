d36bd5d458823fdd25475ae9bde1e84fb4ee8221
Rabin Gaire <rabingaire20@gmail.com>
2017-11-04 10:55:29 +0545

    added action creater and connected it with our component LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import { Card, CardSection, Input, Button } from './common';
+import { emailChanged } from './../actions';
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.onEmailChange = this.onEmailChange.bind(this);
+    }
+
+    onEmailChange(text) {
+        this.props.emailChanged(text);
+    }
+
     render() {
         return(
             <Card>
                 <CardSection>
-                    <Input label="Email" placeholder="email@gmail.com" />
+                    <Input 
+                        label="Email" 
+                        placeholder="email@gmail.com" 
+                        onChangeText={this.onEmailChange} />
                 </CardSection>
 
                 <CardSection>
@@ -23,4 +38,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
